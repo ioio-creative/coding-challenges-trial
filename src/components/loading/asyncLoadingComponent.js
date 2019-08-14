6b1b0e593d2df0742c987bf712a5d7b33b1c6051
hcc1232001 <qes4d26@hotmail.com>
2019-08-14 15:02:42 +0800

    update
    add passing loading status to loading component
    add timeout for component loading
    
    isLoading : indicate is loading
    error: contain error type and error message when fail to load

--- src/components/loading/asyncLoadingComponent.js
+++ src/components/loading/asyncLoadingComponent.js
@@ -4,29 +4,53 @@ import MyFirstLoadingComponent from './MyFirstLoadingComponent';
 
 // https://medium.com/@magbicaleman/intro-to-dynamic-import-in-create-react-app-6305bb397c46
 function Dynamic(props) {
-  const { loaderFunc, loadingComponent, ...rest } = props;
-  const [myModule, setMyModule] = useState(null);
-  useEffect(_ => {
-    loaderFunc()
-      .then((aModule) => {
-        setMyModule(aModule);
+  const { loaderFunc, loadingComponent, timeoutTimeInMs = 5000, ...rest } = props;
+  const [MyModule, setMyModule] = useState(() => loadingComponent);
+  const [componentParams, setComponentParams] = useState({isLoading: true});
+
+  const timeOutPromise = useCallback((_, reject) => {
+    setTimeout(() => {
+      console.log('delay resolve trigger!!!');
+      reject({
+        code: 'TIMEOUT',
+        message: 'Timeout when loading module'
       });
-  }, [loaderFunc]);
-  const Component = myModule ? myModule.default : null;
-  const Loading = loadingComponent;
+    }, timeoutTimeInMs)
+  }, [timeoutTimeInMs]);
+  useEffect(_ => {
+    Promise.race([
+      loaderFunc(),
+      new Promise(timeOutPromise)
+    ])
+    // loaderFunc()
+      .then((aModule) => {
+        // setMyModule(aModule);
+        setComponentParams(rest);
+        setMyModule(()=>aModule.default);
+      }, (error) => {
+        setComponentParams({
+          error: error.code,
+          message: error.message
+        });
+      });
+  }, [loaderFunc, timeOutPromise]);
+  // const Component = myModule ? myModule.default : null;
+  // const Loading = loadingComponent;
   return (
     <>
-      {Component ? <Component {...rest} /> : <Loading />}
+      {/*Component ? <Component {...rest} /> : <Loading />*/}
+      {MyModule && <MyModule {...componentParams} />}
     </>
   );
 }
 
 
-function asyncLoadingComponent(loaderFunc, loadingComponent = MyFirstLoadingComponent) {
+function asyncLoadingComponent(loaderFunc, loadingComponent = MyFirstLoadingComponent, timeoutTimeInMs = 5000) {
   return (props) => (
     <Dynamic
       loaderFunc={loaderFunc}
       loadingComponent={loadingComponent}
+      timeoutTimeInMs = {timeoutTimeInMs}
       {...props}
     />
   );
